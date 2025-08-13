c277b8a624922f92d21acdd96ef908118031b45c
Dapo-Obembe <obembedapo@gmail.com>
2025-08-13 22:59:31 +0100

    Fixed the acw_button working in the block editor area

--- inc/partials/button.php
+++ inc/partials/button.php
@@ -10,19 +10,37 @@
  */
 
  if(!defined('ABSPATH')) exit;
- 
+
+ /**
+ * Outputs a styled HTML anchor tag functioning as a button.
+ *
+ * Supports predefined styling variants (`primary`, `secondary`, `custom`) and allows
+ * appending additional custom classes. Escapes both URL and text content for safety.
+ *
+ * @param string $text    The button text to display.
+ * @param string $url     The URL the button should link to. Defaults to '#'.
+ * @param string $variant The style variant of the button. Accepts 'primary', 'secondary', or 'custom'. Defaults to 'primary'.
+ * @param string $classes Additional CSS classes to append. Optional.
+ *
+ * @return string HTML markup for the button.
+ */
 function awc_button( $text, $url = '#', $variant = 'primary', $classes = '' ) {
-    // Button variants.
-    $variants = [
-        'primary' => 'bg-primary hover:bg-primary/80 text-white py-2 px-8 rounded-full whitespace-nowrap',
-        'secondary' => 'bg-red-600 hover:bg-red-700 text-white py-2 px-8 rounded-full whitespace-nowrap',
-        'custom' => 'py-2 px-8 rounded-full whitespace-nowrap',
-    ];
+	// Button variants.
+	$variants = array(
+		'primary'   => 'primary-button relative z-0',
+		'secondary' => ' secondary-button relative z-0',
+		'custom'    => 'py-2 px-8 rounded-full whitespace-nowrap relative z-0',
+	);
 
-    // Check if variant exists, otherwise use the primary as default.
-    $variant_classes = $variants[$variant] ?? $variants['primary'];
+	// Use admin-safe URL if in the admin area.
+	if ( is_admin() ) {
+		$url = '#';
+	}
 
-    return "<a href='" . esc_url( $url ) . "' role='button' class='$variant_classes $classes'>" . esc_html( $text ) . "</a>";
+	// Check if variant exists, otherwise use the primary as default.
+	$variant_classes = $variants[ $variant ] ?? $variants['primary'];
+
+	return "<a href='" . esc_url( $url ) . "' role='button' class='$variant_classes $classes'>" . esc_html( $text ) . '</a>';
 }
 
 /**
